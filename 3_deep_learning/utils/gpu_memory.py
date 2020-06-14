ee261a62895f4d5edea6be6f7bb387b189887d05
Yoshiaki Nejime <51129480+Shimejime@users.noreply.github.com>
2020-06-14 12:27:25 +0900

    add utils/gpu_memory.py

---
+++ 3_deep_learning/utils/gpu_memory.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+
+def limited_gpu_memory():
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    if len(physical_devices) > 0:
+        for k in range(len(physical_devices)):
+            tf.config.experimental.set_memory_growth(physical_devices[k], True)
+            print('memory growth:', tf.config.experimental.get_memory_growth(physical_devices[k]))
+    else:
+        print("Not enough GPU hardware devices available")
